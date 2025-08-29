99a843420989a47fce47dcfecb4e29d3355d7b58
MatheusAraripe <matheus.alc2001@gmail.com>
2025-08-29 16:45:34 -0300

    feat: adicionando lib para controle manual

--- sketches/sketch02/sketch02.js
+++ sketches/sketch02/sketch02.js
@@ -1,10 +1,20 @@
 import canvasSketch from "canvas-sketch";
 import random from "canvas-sketch-util/random";
 import math from "canvas-sketch-util/math";
+import { Pane } from "tweakpane";
 
 const settings = {
   dimensions: [1080, 1080],
   animate: true,
+};
+
+const params = {
+  cols: 50,
+  rows: 50,
+  scaleMin: 1,
+  scaleMax: 30,
+  frequency: 0.001,
+  amplitude: 0.2,
 };
 
 const sketch = () => {
@@ -12,8 +22,8 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
 
     const numCels = cols * rows;
 
@@ -34,9 +44,11 @@ const sketch = () => {
       const w = cellW * 0.8;
       const h = cellH * 0.8;
 
-      const n = random.noise2D(x + frame * 10, y, 0.001);
-      const angle = n * Math.PI * 0.2;
-      const scale = math.mapRange(n, -1, 1, 1, 30);
+      // const n = random.noise2D(x + frame * 10, y, params.frequency);
+      const n = random.noise3D(x, y, frame * 10, params.frequency);
+
+      const angle = n * Math.PI * params.amplitude;
+      const scale = math.mapRange(n, -1, 1, params.scaleMin, params.scaleMax);
 
       // desenhando grid
       context.save();
@@ -55,5 +67,21 @@ const sketch = () => {
     }
   };
 };
+
+const createPane = () => {
+  const pane = new Pane();
+
+  let folder;
+  folder = pane.addFolder({ title: "Grid" });
+  folder.addInput(params, "cols", { min: 1, max: 100, step: 1 });
+  folder.addInput(params, "rows", { min: 1, max: 100, step: 1 });
+  folder.addInput(params, "scaleMin", { min: 1, max: 100 });
+  folder.addInput(params, "scaleMax", { min: 1, max: 100 });
+
+  folder = pane.addFolder({ title: "Noise" });
+  folder.addInput(params, "frequency", { min: -0.001, max: 0.01 });
+  folder.addInput(params, "amplitude", { min: 0, max: 1 });
+};
+createPane();
 
 canvasSketch(sketch, settings);
