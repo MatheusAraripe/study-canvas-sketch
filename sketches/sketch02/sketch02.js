43bd68d513b1e1cf60d8758837ebeaa85a51ca6a
MatheusAraripe <matheus.alc2001@gmail.com>
2025-08-29 15:10:30 -0300

    feat: ajustando landing page e criando novo sketch(noise)

--- sketches/sketch02/sketch02.js
+++ sketches/sketch02/sketch02.js
@@ -1,13 +1,58 @@
-const canvasSketch = require("canvas-sketch");
+import canvasSketch from "canvas-sketch";
+import random from "canvas-sketch-util/random";
+import math from "canvas-sketch-util/math";
 
 const settings = {
-  dimensions: [2048, 2048],
+  dimensions: [1080, 1080],
+  animate: true,
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, width, height, frame }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    const cols = 10;
+    const rows = 10;
+
+    const numCels = cols * rows;
+
+    const gridW = width * 0.8;
+    const gridH = height * 0.8;
+    const cellW = gridW / cols;
+    const cellH = gridH / rows;
+    const cellMargx = ((width - gridW) * 1) / 2;
+    const cellMargh = ((height - gridH) * 1) / 2;
+
+    for (let i = 0; i < numCels; i++) {
+      // usando módulo para calcular o grid
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cellW;
+      const y = row * cellH;
+      const w = cellW * 0.8;
+      const h = cellH * 0.8;
+
+      const n = random.noise2D(x + frame * 10, y, 0.001);
+      const angle = n * Math.PI * 0.2;
+      const scale = math.mapRange(n, -1, 1, 1, 30);
+
+      // desenhando grid
+      context.save();
+      context.translate(x, y);
+      context.translate(cellMargx, cellMargh);
+      context.translate(cellW * 0.5, cellH * 0.5);
+      context.rotate(angle);
+
+      context.lineWidth = scale;
+
+      context.beginPath();
+      context.moveTo(w * -0.5, 0);
+      context.lineTo(w * 0.5, 0);
+      context.stroke();
+      context.restore();
+    }
   };
 };
 
